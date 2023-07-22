c790afa184e825854d47e99b63bd81ecd3dd4851
koraykoksal <kksl.koray@gmail.com>
2023-07-22 13:46:31 +0300

    loginProvider

---
+++ context-example2/src/context/LoginProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react"
+
+//! Login Context'i olusuturuldu
+export const LoginContext = createContext()
+
+//sarmalayıcı bir component oluşturuldu.
+//react da her componentin children adında bir propsuv vardır.
+
+const values={
+    user,
+    setUser,
+}
+
+export const LoginProvider = ({children}) => {
+
+
+  return (
+
+    <LoginContext value={{values}}>
+        {children}
+    </LoginContext>
+  )
+}
